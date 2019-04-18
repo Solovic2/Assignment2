0d9699aaff26a0316534babb1526e703a7a2470b
Solovic2 <45718998+Solovic2@users.noreply.github.com>
2019-04-18 16:45:57 +0300

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,9 @@
+package assignment;
+
+
+import assignment.node;
+import static java.time.Clock.system;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +14,147 @@
  *
  * @author Momen
  */
+
 public class LinkedList {
-    node
+    private  node head=null ;
+   // private node node;
+    
+    public void add (char c, int index){
+        node i=head;
+        node c1=new node();
+        c1.value=c;
+        if(index==0){
+            c1.next=head;
+            head=c1;
+        }else{
+           for(int count=0;count <index-1 ;count++){
+                   i=i.next;
+            }  
+            c1.next=i.next;
+            i.next=c1;
+        }
+    }
+    public void add(char c){
+        node i=head;
+        node c2=new node();
+        c2.value=c;
+        while(i!=null){
+            if(i.next==null){
+                c2.next=null;
+                i.next=c2;
+                i=null;
+            }else{
+                i=i.next;
+            }
+        }
+        
+    }
+    public void print(){
+        node i=head;
+       while(i!=null){
+        System.out.println(i.value);
+        i=i.next;
+       }
+    }
+    public  int  getlength(){
+        node i=head;
+        int length=0;
+        while(i!=null){
+            length++;
+            i=i.next;
+        }
+        return length;
+    }
+    public char get(int index){
+      node i=head;
+        int counter;
+       counter= getlength();
+        if(index==0){
+           return head.value;
+        }
+        else if(index>counter)
+        {
+            System.out.println("You Choose A Null Character");
+            return ' ' ;
+        }
+        else
+        {
+            for(int count =0;count<index;count++){
+                i=i.next;
+            }
+            return i.value;
+        }
+    }
+    public void set(int index, char c){
+        int counter;
+        counter=getlength();
+            node i=head;
+        if(index==0){
+           head.value=c;
+        }
+        else if(index>counter)
+        {
+            System.out.println("You Choose A Null Character");
+            
+        }
+        else
+        {
+            for(int count =0;count<index;count++){
+                i=i.next;
+            }
+            i.value=c;
+        }
+        
+    }
+    public void clear(){
+        node i=head;
+        head=null;
+        i.next =null;
+    }
+    public boolean isEmpty(){
+        node i=head;
+        if(i==null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void remove(int index){
+        node i=head;
+        int counter;
+        counter=getlength();
+        node j=i.next;
+        if(index==0){
+          head=head.next;
+        }else if(index==counter-1){
+            for(int count =0;count<index-1;count++){
+                i=i.next;
+                j=j.next;
+            } 
+            i.next=j.next;
+            j.next=null;
+        }
+        else
+        {
+           for(int count=0;count<index-1;count++){
+                i=i.next;
+                j=j.next;
+            }
+            i.next=j.next;
+            j.next=null;
+        }
+    }
+    public boolean contains(char c){
+        node i=head;
+        while(i!=null){
+            if(i.value==c)
+            {
+                return true;
+            }else 
+                i=i.next;
+            
+        }
+        return false;
+    }
+
 }
